433c85917ba8f0a015815bd4e05c4430015c7255
Ananya Sahu <ananyasahu59513@gmail.com>
2025-02-06 23:17:22 +0530

    loops in java

---
+++ Loop.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+public class Loop{
+    public static void main(String args[]){
+        Scanner obj = new Scanner(System.in);
+
+
+        //two's table
+
+        // int n=2;
+
+        // for(int i=1;i<=10;i++){
+        //       System.out.println(i*n);
+        // }
+       
+        // //tables from 2 to 10
+
+        // for(int i=2;i<=10;i++){
+        //     for(int j=1;j<=10;j++){
+        //         System.out.println(i*j);
+        //     }
+        // }
+
+
+
+        //Factorial of n natural numbers
+// int n=6;
+// int sum=1;
+// for(int i=1;i<=n;i++){
+//     sum*=i;
+   
+// }
+//  System.out.println(sum);
+//     }
+// }
+
+
+//factor
+
+int a=9;
+
+for(int i=1;i<a;i++){
+    if(a%i==0){
+     System.out.println(i);
+    }   
+}
+}
+}
+
